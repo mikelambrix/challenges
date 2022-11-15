1e84630d117e6ad24771ffee28ddbc9083b95781
Mike Lambrix <mlambrix2@gmail.com>
2022-11-14 21:25:57 -0800

    sol 5

---
+++ Solution-11.14.2022-5.js
@@ -0,0 +1,8 @@
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+    const num1 = Number(a);
+    const num2 = Number(b);
+    return String(num1 + num2)
+  }
+  
